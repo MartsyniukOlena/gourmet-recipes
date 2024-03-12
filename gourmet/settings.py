1eea4cc3b14a7970d067c37c8a7f2270006410f5
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-12 14:45:47 +0000

    Install database

--- gourmet/settings.py
+++ gourmet/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure-i)(dt@u*4t65w4+xovf8=i63e4b!n^#k@gcfstqb^@_qa(x%n7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-martsyniuko-gourmetreci-wzob0cyyj84.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-martsyniuko-gourmetreci-wzob0cyyj84.ws-eu108.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -74,11 +79,15 @@ WSGI_APPLICATION = 'gourmet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 

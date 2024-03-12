9f0eb5bb06a43e841d26dce4041612c2c556898a
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-12 15:54:46 +0000

    Add generic recipes view

--- gourmet/urls.py
+++ gourmet/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from recipes import views as recipe_views
 
 urlpatterns = [
+    path("recipes/", include("recipes.urls"), name="recipes-urls"),
     path('admin/', admin.site.urls),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
 # Create your views here.
+class RecipeList(generic.ListView):
+    queryset = Recipe.objects.filter(status=1)
+    template_name = "recipes/all_recipes.html"
+    paginate_by = 3

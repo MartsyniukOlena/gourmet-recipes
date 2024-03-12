a0a251e71ad518fbf8e844d277af651ad46c61ca
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-12 15:24:35 +0000

    Migrate Recipe, Comment, Category models

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Recipe, Comment, Category
 
 # Register your models here.
-admin.site.register(Comment)
-admin.site.register(Category)
 admin.site.register(Recipe)
+admin.site.register(Category)
+admin.site.register(Comment)
 
